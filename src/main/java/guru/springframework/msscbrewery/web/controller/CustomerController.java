b06e342d93e03ac77c0201f338d6aec5084986d4
Mathias Schulze <mathias-schulze@web.de>
2023-07-08 16:56:23 +0200

    assignment 2

--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -2,17 +2,16 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.CustomerService;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RequestMapping("/api/v1/customer")
 @RestController
+@Slf4j
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -24,5 +23,32 @@ public class CustomerController {
     public ResponseEntity<CustomerDto> getBeer(@PathVariable("customerId") UUID customerId){
 
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity addCustomer(@RequestBody CustomerDto customer) {
+
+        log.debug("Customer created: " + customer.getName() + " (" + customer.getId() + ")");
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{customerId}"})
+    public ResponseEntity updateCustomer(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customer) {
+
+        CustomerDto customerById = customerService.getCustomerById(customerId);
+
+        log.debug("customer name changed from " + customerById.getName() + " to " + customer.getName());
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping({"/{customerId}"})
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCustomer(@PathVariable("customerId") UUID customerId) {
+
+        CustomerDto customerById = customerService.getCustomerById(customerId);
+
+        log.debug("customer " + customerById.getName() + " deleted");
     }
 }
